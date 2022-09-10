6c4f32920fc23ec8ff0802d2585b706d3dae3feb
Suraj Gaire <eriag321@gmail.com>
2022-09-10 10:05:53 +0545

    readme updated

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,11 +1,12 @@
 const CACHE_NAME = "version-1";
-const urlToCache = ["./index.html", "/", "./offline.html"];
+const urlToCache = ["./index.html", "/", "./offline.html","/static/js","/static/css"];
 const Dynamic_Cache_Name = "dynamic-version-v1";
 
 const self = this;
 
 //install service worker
 self.addEventListener("install", (e) => {
+  // console.log(e)
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("opened cache");
@@ -16,21 +17,22 @@ self.addEventListener("install", (e) => {
 
 //listen for request
 self.addEventListener("fetch", (e) => {
+  console.log(e.request.url);
   e.respondWith(
     caches
       .match(e.request)
       .then((res) => {
         return (
           res ||
-          fetch(e.request).then((res) => {
-            return caches.open(Dynamic_Cache_Name).then((cache) => {
-              cache.put(e.request.url, res.clone());
-              return res;
-            });
+          fetch(e.request).then(async (res) => {
+            const cache = await caches.open(Dynamic_Cache_Name);
+            cache.put(e.request.url, res.clone());
+            return res;
           })
         );
       })
-      .catch(() => {
+      .catch((error) => {
+        console.log(error.message);
         if (e.request.url.indexof(".html") > -1) {
           return caches.match("./offline.html");
         }
