7775cdd93b187748f2fdb12fad67af62b890eb55
Suraj Gaire <eriag321@gmail.com>
2022-11-07 20:26:43 +0545

    left file added for sw

---
+++ src/sw.js
@@ -0,0 +1,6 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+
+// register routes
+workbox.routing.registerRoute(new RegExp())
+
+workbox.precaching.precacheAndRoute([]) 
